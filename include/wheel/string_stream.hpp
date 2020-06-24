ab11032e8b946e19e4d29ab8085eb1b0a0176084
CodingMaster <64177352+Yinpinghua@users.noreply.github.com>
2020-06-24 17:08:29 +0800

    Update string_stream.hpp

--- include/wheel/string_stream.hpp
+++ include/wheel/string_stream.hpp
@@ -1,88 +1,42 @@
 #ifndef string_stream_h__
 #define string_stream_h__
 
-#include <string>
-namespace wheel {
+#include "common.hpp"
+
+namespace super_dns {
 	namespace str_stream {
 		template <typename alloc_ty>
-		struct basic_string_stream
-		{
-		private:
-			alloc_ty alloc;
+		class basic_string_stream {
 		public:
-			enum { good, read_overflow };
-
-			char* m_header_ptr;
-			char* m_read_ptr;
-			char* m_write_ptr;
-			char* m_tail_ptr;
-			int			m_status;
-			std::size_t	m_length;
-
-			enum { INIT_BUFF_SIZE = 1024 };
-
-			basic_string_stream()
-				: m_length(INIT_BUFF_SIZE)
-				, m_status(good)
-			{
-				this->m_header_ptr = this->alloc.allocate(INIT_BUFF_SIZE);
-				this->m_read_ptr = this->m_header_ptr;
-				this->m_write_ptr = this->m_header_ptr;
-				this->m_tail_ptr = this->m_header_ptr + m_length;
+			basic_string_stream() {
+				header_ptr_ = this->alloc_.allocate(INIT_BUFF_SIZE);
+				memset(&header_ptr_[0], 0, INIT_BUFF_SIZE);
+				this->read_ptr_ = this->header_ptr_;
+				this->write_ptr_ = this->header_ptr_;
+				this->tail_ptr_ = this->header_ptr_ + length_;
 			}
 
-			~basic_string_stream()
-			{
-				this->alloc.deallocate(m_header_ptr, this->m_length);
+			~basic_string_stream() {
+				clear();
+				this->alloc_.deallocate(header_ptr_, this->length_);
 			}
 
-			inline std::size_t write(const char* buffer)
-			{
+			inline std::size_t write(const char* buffer) {
 				return write(buffer, strlen(buffer));
 			}
 
-			inline std::size_t read(const char* buffer, std::size_t len)
-			{
-				if (this->m_read_ptr + len > this->m_tail_ptr)
+			inline std::size_t read(const char* buffer, std::size_t len) {
+				if (this->read_ptr_ + len > this->tail_ptr_)
 				{
-					m_status = read_overflow;
+					status_ = read_overflow;
 					return 0;
 				}
-				std::memcpy(buffer, this->m_read_ptr, len);
-				this->m_read_ptr += len;
+				std::memcpy(buffer, this->read_ptr_, len);
+				this->read_ptr_ += len;
 				return len;
 			}
 
-			inline std::size_t growpup(std::size_t want_size)
-			{
-				std::size_t new_size = ((want_size + INIT_BUFF_SIZE - 1) / INIT_BUFF_SIZE) * INIT_BUFF_SIZE;
-				std::size_t write_pos = this->m_write_ptr - this->m_header_ptr;
-				std::size_t read_pos = this->m_read_ptr - this->m_header_ptr;
-				char* temp = this->m_header_ptr;
-				this->m_header_ptr = this->alloc.allocate(new_size);
-				std::memcpy(this->m_header_ptr, temp, this->m_length);
-				this->alloc.deallocate(temp, this->m_length);
-				this->m_length = new_size;
-				this->m_write_ptr = this->m_header_ptr + write_pos;
-				this->m_read_ptr = this->m_header_ptr + read_pos;
-				this->m_tail_ptr = this->m_header_ptr + m_length;
-				return new_size;
-			}
-
-			inline std::size_t write(const char* buffer, std::size_t len)
-			{
-				std::size_t writed_len = this->m_write_ptr + len - this->m_header_ptr;
-				if (writed_len > this->m_length)
-				{
-					this->growpup(writed_len);
-				}
-				std::memcpy((void*)this->m_write_ptr, buffer, len);
-				this->m_write_ptr += len;
-				return len;
-			}
-
-			inline void write_str(char const* str, size_t len)
-			{
+			inline void write_str(char const* str, size_t len) {
 				put('"');
 				char const* ptr = str;
 				char const* end = ptr + len;
@@ -92,28 +46,28 @@ namespace wheel {
 					if (c == 0)
 						break;
 					++ptr;
-					if (escape[(unsigned char)c])
+					if (escape_[(unsigned char)c])
 					{
 						char buff[6] = { '\\', '0' };
 						size_t len = 2;
-						buff[1] = escape[(unsigned char)c];
+						buff[1] = escape_[(unsigned char)c];
 						if (buff[1] == 'u')
 						{
 							if (ptr < end)
 							{
-								buff[2] = (hex_table[((unsigned char)c) >> 4]);
-								buff[3] = (hex_table[((unsigned char)c) & 0xF]);
+								buff[2] = (hex_table_[((unsigned char)c) >> 4]);
+								buff[3] = (hex_table_[((unsigned char)c) & 0xF]);
 								const char c1 = *ptr;
 								++ptr;
-								buff[4] = (hex_table[((unsigned char)c1) >> 4]);
-								buff[5] = (hex_table[((unsigned char)c1) & 0xF]);
+								buff[4] = (hex_table_[((unsigned char)c1) >> 4]);
+								buff[5] = (hex_table_[((unsigned char)c1) & 0xF]);
 							}
 							else
 							{
 								buff[2] = '0';
 								buff[3] = '0';
-								buff[4] = (hex_table[((unsigned char)c) >> 4]);
-								buff[5] = (hex_table[((unsigned char)c) & 0xF]);
+								buff[4] = (hex_table_[((unsigned char)c) >> 4]);
+								buff[5] = (hex_table_[((unsigned char)c) & 0xF]);
 							}
 							len = 6;
 						}
@@ -127,48 +81,80 @@ namespace wheel {
 				put('"');
 			}
 
-			inline void put(char c)
-			{
-				std::size_t writed_len = this->m_write_ptr + 1 - this->m_header_ptr;
-				if (writed_len > this->m_length)
-				{
-					this->growpup(writed_len);
-				}
-				*this->m_write_ptr = c;
-				++this->m_write_ptr;
+			inline bool bad()const { return status_ != good; }
+
+			inline const char* data() const {
+				return this->header_ptr_;
 			}
 
-			inline bool bad()const { return m_status != good; }
-
-			inline void clear()
-			{
-				this->m_read_ptr = this->m_header_ptr;
-				this->m_write_ptr = this->m_header_ptr;
-			}
-
-			inline const char* data() const
-			{
-				return this->m_header_ptr;
-			}
-
-			std::basic_string<char, std::char_traits<char>, alloc_ty> str()
-			{
-				std::basic_string<char, std::char_traits<char>, alloc_ty> s(this->m_header_ptr, this->write_length());
+			std::basic_string<char, std::char_traits<char>, alloc_ty> str() {
+				std::basic_string<char, std::char_traits<char>, alloc_ty> s(this->header_ptr_, this->write_length());
 				return s;
 			}
 
-			inline ::std::size_t read_length() const
-			{
-				return this->m_read_ptr - this->m_header_ptr;
+			inline void put(char c) {
+				std::size_t writed_len = this->write_ptr_ + 1 - this->header_ptr_;
+				if (writed_len > this->length_)
+				{
+					this->growpup(writed_len);
+				}
+				*this->write_ptr_ = c;
+				++this->write_ptr_;
 			}
 
-			inline ::std::size_t write_length() const
-			{
-				return this->m_write_ptr - this->m_header_ptr;
+			inline std::size_t write(const char* buffer, std::size_t len) {
+				std::size_t writed_len = this->write_ptr_ + len - this->header_ptr_;
+				if (writed_len > this->length_)
+				{
+					this->growpup(writed_len);
+				}
+				std::memcpy((void*)this->write_ptr_, buffer, len);
+				this->write_ptr_ += len;
+				return len;
+			}
+		private:
+			inline void clear() {
+				this->read_ptr_ = this->header_ptr_;
+				this->write_ptr_ = this->header_ptr_;
 			}
 
-			char const* hex_table = "0123456789ABCDEF";
-			char const escape[256] = {
+			inline ::std::size_t read_length() const {
+				return this->read_ptr_ - this->header_ptr_;
+			}
+
+			inline ::std::size_t write_length() const {
+				return this->write_ptr_ - this->header_ptr_;
+			}
+
+			inline std::size_t growpup(std::size_t want_size) {
+				std::size_t new_size = ((want_size + INIT_BUFF_SIZE - 1) / INIT_BUFF_SIZE) * INIT_BUFF_SIZE;
+				std::size_t write_pos = this->write_ptr_ - this->header_ptr_;
+				std::size_t read_pos = this->read_ptr_ - this->header_ptr_;
+				char* temp = this->header_ptr_;
+				this->header_ptr_ = this->alloc_.allocate(new_size);
+				std::memcpy(this->header_ptr_, temp, this->length_);
+				this->alloc_.deallocate(temp, this->length_);
+				this->length_ = new_size;
+				this->write_ptr_ = this->header_ptr_ + write_pos;
+				this->read_ptr_ = this->header_ptr_ + read_pos;
+				this->tail_ptr_ = this->header_ptr_ + length_;
+				return new_size;
+			}
+		private:
+			alloc_ty alloc_;
+			enum { good, read_overflow };
+			enum { INIT_BUFF_SIZE = 1024 };
+
+
+			char* header_ptr_;
+			char* read_ptr_;
+			char* write_ptr_;
+			char* tail_ptr_;
+			int			status_ = good;
+			std::size_t	length_ = INIT_BUFF_SIZE;
+
+			char const* hex_table_ = "0123456789ABCDEF";
+			char const escape_[256] = {
 		#define Z16 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
 				//0    1    2    3    4    5    6    7    8    9    A    B    C    D    E    F
 				'u', 'u', 'u', 'u', 'u', 'u', 'u', 'u', 'b', 't', 'n', 'u', 'f', 'r', 'u', 'u', // 00
